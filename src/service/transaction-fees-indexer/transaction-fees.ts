b77cedace15d04ffbd92373dafb41f2c2ae9d2d6
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-03-24 16:49:23 +0100

    fix mistake from past commit

--- src/service/transaction-fees-indexer/transaction-fees.ts
+++ src/service/transaction-fees-indexer/transaction-fees.ts
@@ -70,7 +70,7 @@ export async function computeAccumulatedTxFeesByAddress({
 
       const currGasFeePSP = new BigNumber(swap.txGasUsed.toString())
         .multipliedBy(swap.txGasPrice.toString()) // in gwei
-        .multipliedBy(1e-9) //  convert to wei
+        .multipliedBy(1e9) //  convert to wei
         .multipliedBy(pspRateSameDay);
 
       const accGasFeePSP = (swapperAcc?.accGasFeePSP || new BigNumber(0)).plus(
