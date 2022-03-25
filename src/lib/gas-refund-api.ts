c251bd0a66989d7fe97740d1dff3f93560a8476b
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-03-25 20:00:32 +0100

    feat/gas-refund: implement filtering of already claimed refund

--- src/lib/gas-refund-api.ts
+++ src/lib/gas-refund-api.ts
@@ -1,28 +1,50 @@
 // import GasRefundModel from '../models/GasRefund';
-import { Interface } from '@ethersproject/abi';
-import {
-  TransactionRequest,
-  TransactionResponse,
-} from '@ethersproject/providers';
+import { TransactionRequest } from '@ethersproject/providers';
+import { Contract } from 'ethers';
 import _ from 'lodash';
+import { assert } from 'ts-essentials';
 import { getMerkleTree } from '../service/transaction-fees-indexer/persistance';
 import {
   MerkleData,
   MerkleRoot,
 } from '../service/transaction-fees-indexer/types';
-import { CHAIN_ID_MAINNET } from './constants';
+import {
+  CHAIN_ID_BINANCE,
+  CHAIN_ID_FANTOM,
+  CHAIN_ID_MAINNET,
+  CHAIN_ID_POLYGON,
+} from './constants';
 import { EpochInfo } from './epoch-info';
+import { Provider } from './provider';
 
 const MerkleRedeemAbi = [
   'function seedAllocations(uint _week, bytes32 _merkleRoot, uint _totalAllocation)',
+  'function claimStatus(address _liquidityProvider, uint _begin, uint _end) external view returns (bool[] memory)',
 ];
 
-const IfaceMerkleRedeem = new Interface(MerkleRedeemAbi);
+interface MerkleRedeem extends Contract {
+  callStatic: {
+    claimStatus(
+      _liquidityProvider: string,
+      _begin: number,
+      _end: number,
+    ): Promise<boolean[]>;
+  };
+}
 
-const GasRefundGenesisEpoch = 7; // @FIXME
+const MerkleRedeemAddress: { [chainId: number]: string } = {
+  // @TODO
+  [CHAIN_ID_MAINNET]: '0x',
+  [CHAIN_ID_POLYGON]: '0xe4aa70d4b77533000dc51bc4b98f26f4ee1aaea4', // @FIXME
+  [CHAIN_ID_FANTOM]: '0x',
+  [CHAIN_ID_BINANCE]: '0x',
+};
+
+const GasRefundGenesisEpoch = 8; // @FIXME
 
 export class GasRefundApi {
   epochInfo: EpochInfo;
+  merkleRedem: MerkleRedeem;
   // gasRefundModel: GasRefundModel;
 
   static instances: { [network: number]: GasRefundApi } = {};
@@ -30,6 +52,11 @@ export class GasRefundApi {
   constructor(protected network: number) {
     this.epochInfo = EpochInfo.getInstance(CHAIN_ID_MAINNET);
     // this.gasRefundModel = new GasRefundModel(network);
+    this.merkleRedem = new Contract(
+      MerkleRedeemAddress[network],
+      MerkleRedeemAbi,
+      Provider.getJsonRpcProvider(this.network),
+    ) as unknown as MerkleRedeem;
   }
 
   static getInstance(network: number): GasRefundApi {
@@ -55,11 +82,10 @@ export class GasRefundApi {
 
     const { root } = merkleTree;
 
-    const txData = IfaceMerkleRedeem.encodeFunctionData('seedAllocations', [
-      lastEpochNum,
-      root.merkleRoot,
-      root.totalAmount,
-    ]);
+    const txData = this.merkleRedem.interface.encodeFunctionData(
+      'seedAllocations',
+      [lastEpochNum, root.merkleRoot, root.totalAmount],
+    );
 
     return {
       root,
@@ -71,17 +97,15 @@ export class GasRefundApi {
     };
   }
 
-  // get all ever constructed merkle data for addrress
-  // @FIXME: filter already claimed
-  async getMerkleDataForAddress(address: string): Promise<MerkleData[] | null> {
-    const lastEpochNum = (await this.epochInfo.getCurrentEpoch()) - 1;
-    
+  async _fetchMerkleData(
+    address: string,
+    startEpoch: number,
+    endEpoch: number,
+  ): Promise<MerkleData[]> {
     const epochs =
-      lastEpochNum < GasRefundGenesisEpoch
-        ? []
-        : lastEpochNum === GasRefundGenesisEpoch
+      startEpoch === endEpoch
         ? [GasRefundGenesisEpoch]
-        : _.range(GasRefundGenesisEpoch, lastEpochNum);
+        : _.range(startEpoch, endEpoch + 1);
 
     const merkleData = await Promise.all(
       epochs.map(async epochNum => {
@@ -101,5 +125,47 @@ export class GasRefundApi {
     );
 
     return merkleData.filter(v => !!v) as MerkleData[]; // @fixme types do not work
+  }
+
+  async _getClaimStatus(
+    address: string,
+    startEpoch: number,
+    endEpoch: number,
+  ): Promise<Record<number, boolean>> {
+    const claimStatus = await this.merkleRedem.callStatic.claimStatus(
+      address,
+      startEpoch,
+      endEpoch,
+    );
+
+    const epochToClaimed = claimStatus.reduce<Record<number, boolean>>(
+      (acc, claimed, index) => {
+        acc[startEpoch + index] = claimed;
+        return acc;
+      },
+      {},
+    );
+
+    assert(
+      Object.keys(epochToClaimed).length == endEpoch - startEpoch + 1,
+      'logic error',
+    );
+
+    return epochToClaimed;
+  }
+
+  // get all ever constructed merkle data for addrress
+  async getMerkleDataForAddress(address: string): Promise<MerkleData[] | null> {
+    const lastEpoch = (await this.epochInfo.getCurrentEpoch()) - 1;
+
+    const startEpoch = GasRefundGenesisEpoch;
+    const endEpoch = Math.max(lastEpoch, GasRefundGenesisEpoch);
+
+    const [merkleData, epochToClaimed] = await Promise.all([
+      this._fetchMerkleData(address, startEpoch, endEpoch),
+      this._getClaimStatus(address, startEpoch, endEpoch),
+    ]);
+
+    return merkleData.filter(m => !epochToClaimed[m.epoch]);
   }
 }
