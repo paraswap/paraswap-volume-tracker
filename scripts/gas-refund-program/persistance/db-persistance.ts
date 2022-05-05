c919ef83a942d3b2c1117052b208b285d92a4ea3
Sam <samt@samt.st>
2022-05-05 23:55:32 +0100

    remove obsolete

--- scripts/gas-refund-program/persistance/db-persistance.ts
+++ scripts/gas-refund-program/persistance/db-persistance.ts
@@ -186,15 +186,3 @@ export const saveMerkleTreeInDB = async ({
     merkleRoot,
   });
 };
-
-export const fetchTransactionOccurences = async (epoch: number, chainId: number): Promise<Record<string, number>> => {
-  const txOccurences: Record<string, number> = {}
-  const txs: Pick<GasRefundTransactionData, 'hash' | 'occurence'>[] = await GasRefundTransaction.findAll({
-    where: { chainId, epoch },
-    attributes: ['hash', 'occurence']
-  })
-  txs.forEach((tx) => {
-    txOccurences[tx.hash] = tx.occurence
-  })
-  return txOccurences
-}
