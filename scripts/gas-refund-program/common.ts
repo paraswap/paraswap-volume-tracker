690b2c8c71b1b360ea4c8c0b0e1897ead6f2a9e3
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-05-10 09:18:32 +0200

    add comment

--- scripts/gas-refund-program/common.ts
+++ scripts/gas-refund-program/common.ts
@@ -12,7 +12,7 @@ export async function init(options?: Params) {
   await epochInfo.getEpochInfo();
 }
 
-export const SCRIPT_START_TIME_SEC = Math.round(Date.now() / 1000);
+export const SCRIPT_START_TIME_SEC = Math.round(Date.now() / 1000); // stable script start time to align stakes and transactions fetching time intervals
 export const OFFSET_CALC_TIME = 5 * 60; // delay to ensure that all third parties providers are synced
 
 export async function resolveEpochCalcTimeInterval(epoch: number): Promise<{
