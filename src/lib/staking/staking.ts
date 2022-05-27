6d17352d7a843bd183c9db434379121bb810f9c1
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-05-27 18:02:08 +0200

    fix blocknum for all queries

--- src/lib/staking/staking.ts
+++ src/lib/staking/staking.ts
@@ -1,8 +1,9 @@
-import { SAFETY_MODULE_ADDRESS } from '../constants';
+import { CHAIN_ID_MAINNET, SAFETY_MODULE_ADDRESS } from '../constants';
 import { SPSPHelper } from './spsp-helper';
 import { SafetyModuleHelper } from './safety-module-helper';
 import { assert } from 'ts-essentials';
 import { PSPStakesAllStakers, PSPStakesForStaker } from './types';
+import { Provider } from '../provider';
 
 export class StakingService {
   static instance: StakingService;
@@ -36,13 +37,18 @@ export class StakingService {
   };
 
   getAllPSPStakersAllPrograms = async (
-    blockNumber?: number,
+    _blockNumber?: number,
   ): Promise<{
+    blockNumber: number;
     totalPSPStaked: string;
     totalPSPStakedSPSP: string;
     totalPSPStakedStkPSPBPt: string;
     pspStakersWithStake: PSPStakesAllStakers<string>;
   }> => {
+    const blockNumber =
+      _blockNumber ||
+      (await Provider.getJsonRpcProvider(CHAIN_ID_MAINNET).getBlockNumber());
+
     const [sPSPStakers, stkPSPBPtStakers] = await Promise.all([
       SPSPHelper.getInstance().fetchPSPStakedInSPSP(blockNumber),
       SafetyModuleHelper.getInstance().fetchPSPStakedInStkPSPBpt(blockNumber),
@@ -122,6 +128,7 @@ export class StakingService {
     );
 
     return {
+      blockNumber,
       totalPSPStaked: totalPSPStaked.toString(),
       totalPSPStakedSPSP: totalPSPStakedSPSP.toString(),
       totalPSPStakedStkPSPBPt: totalPSPStakedStkPSPBPt.toString(),
