79b98fd3c37134aafcde96ecb702f618644fe47c
Mohamed Hassen Mami <mohamed@paraswap.io>
2022-11-21 15:30:35 +0100

    add fixme

--- scripts/gas-refund-program/transactions-validation/validateTransactions.ts
+++ scripts/gas-refund-program/transactions-validation/validateTransactions.ts
@@ -34,7 +34,8 @@ import {
  * - update in memory budget accountability through budgetGuardian on validated transactions
  * - write back status of tx in database if changed
  */
-//FIXME: handle overflow of 100% refund for sePSP2
+// FIXME: handle overflow of 100% refund for sePSP2
+// FIXME: handle double spending issue with 100% refund for sePSP2
 export async function validateTransactions() {
   const guardian = GRPBudgetGuardian.getInstance();
 
