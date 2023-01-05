9e61a7c82d6052c88caf10d4a569dea4bd2e114e
Mohamed Hassen Mami <mohamed@paraswap.io>
2023-01-05 13:00:46 +0100

    even more logs

--- src/lib/provider.ts
+++ src/lib/provider.ts
@@ -1,12 +1,15 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Web3Provider } from './constants';
 
+const logger = global.LOGGER('provider')
+
 export class Provider {
   static jsonRpcProviders: { [network: number]: JsonRpcProvider } = {};
   static getJsonRpcProvider(network: number): JsonRpcProvider {
     if (!this.jsonRpcProviders[network]) {
       if (!Web3Provider[network])
         throw new Error(`Provider not defined for network ${network}`);
+        logger.info(`found for chainId=${network}, providerURL=${Web3Provider[network]}`)
       this.jsonRpcProviders[network] = new JsonRpcProvider(
         Web3Provider[network],
       );
