e9b368240cd5d867981245506bf2324eca0108d5
Mohamed Hassen Mami <mohamed@paraswap.io>
2023-02-19 15:00:05 +0100

    GRP: fix issue when running script since beginning of epoch as covalent api misbehave

--- scripts/gas-refund-program/transactions-indexing/txs-covalent.ts
+++ scripts/gas-refund-program/transactions-indexing/txs-covalent.ts
@@ -58,7 +58,9 @@ export const covalentGetTXsForContract = async ({
       throw new Error('only query historic data');
     }
 
-    return `/${chainId}/address/${contract}/transactions_v2/?key=${COVALENT_API_KEY}&no-logs=true&page-number=${page}&page-size=1000&block-signed-at-limit=${startSecondsAgo}&block-signed-at-span=${duration}&match={"to_address": "${contract}"}`;
+    // pagination params (page-size, block-signed-at-limit, block-signed-at-span) don't play nice alltogether
+    // specifically for sePSP2 as the nb of total txs was > page-size=1000 this broke.
+    return `/${chainId}/address/${contract}/transactions_v2/?key=${COVALENT_API_KEY}&no-logs=true&page-number=${page}&page-size=5000&block-signed-at-limit=${startSecondsAgo}&block-signed-at-span=${duration}&match={"to_address": "${contract}"}`;
   };
 
   // todo: better would be to first call the end point with page-size=0 just to get the total number of items, and then construct many request promises and run concurrently - currently this isn't possible (as `total_count` is null) in the covalent api but scheduled
