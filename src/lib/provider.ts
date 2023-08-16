5b6ab0038719353eb94160be07bd50ebab3dc84b
alexshchur <60445720+alexshchur@users.noreply.github.com>
2023-08-16 19:41:32 +0300

    work around timeout bug i was facing in dev mode which halted crontask execution

--- src/lib/provider.ts
+++ src/lib/provider.ts
@@ -1,16 +1,22 @@
-import { JsonRpcProvider } from '@ethersproject/providers';
+import {
+  JsonRpcBatchProvider,
+  JsonRpcProvider,
+} from '@ethersproject/providers';
 import { Web3Provider } from './constants';
 
+const TEN_SECONDS_MS = 10 * 1000;
 export class Provider {
   static jsonRpcProviders: { [network: number]: JsonRpcProvider } = {};
   static getJsonRpcProvider(network: number): JsonRpcProvider {
     if (!this.jsonRpcProviders[network]) {
       if (!Web3Provider[network])
         throw new Error(`Provider not defined for network ${network}`);
-      this.jsonRpcProviders[network] = new JsonRpcProvider(
-        Web3Provider[network],
-      );
+      this.jsonRpcProviders[network] = new JsonRpcBatchProvider({
+        url: Web3Provider[network],
+        timeout: TEN_SECONDS_MS,
+      });
     }
+
     return this.jsonRpcProviders[network];
   }
 }
