fd7f51bb7a4c4926f7b8b7e3cdd9a7905db46319
Mohamed-Hassen <mohamedhassen.mami@gmail.com>
2023-09-11 12:57:33 +0200

    Update scripts/gas-refund-program/pip38/index.ts
    Co-authored-by: alexshchur <60445720+alexshchur@users.noreply.github.com>

--- scripts/gas-refund-program/pip38/index.ts
+++ scripts/gas-refund-program/pip38/index.ts
@@ -35,14 +35,14 @@ export function composeRefundWithPIP38Refunds(
   return allAccounts.reduce<AddressRewards[]>((acc, account) => {
     const epochRefundsMultipleChains = epochRefundsByAddress[account];
 
-    assert(
-      epoch37FantomRefundByAddress[account]?.length < 1,
-      'each account should have at most one record for epoch 37 fantom',
-    );
-    assert(
-      epoch38FantomRefundByAddress[account]?.length < 1,
-      'each account should have at most one record for epoch 38 fantom',
-    );
+     assert(
+        (epoch37FantomRefundByAddress[account]?.length ?? 0) <= 1,
+        'each account should have at most one record for epoch 37 fantom',
+      );
+      assert(
+        (epoch38FantomRefundByAddress[account]?.length ?? 0) <= 1,
+        'each account should have at most one record for epoch 38 fantom',
+      );
 
     const epoch37FantomRefund = epoch37FantomRefundByAddress[account]?.[0];
     const epoch38FantomRefund = epoch38FantomRefundByAddress[account]?.[0];
