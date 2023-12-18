78cadd257614b3d121859059811d68fe61de6bb5
alexshchur <60445720+alexshchur@users.noreply.github.com>
2023-12-18 12:01:30 +0200

    chore: extract calls to staking-supervisor into a lib for reusing

---
+++ src/lib/utils/staking-supervisor.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { assert } from 'ts-essentials';
+
+export type MinParaBoostData = {
+  account: string;
+  score: string;
+  stakesScore: string;
+  sePSP1UnderlyingPSPBalance: string;
+  sePSP2UnderlyingPSPBalance: string;
+  claimableSePSP1Balance: string;
+  paraBoostFactor: string;
+};
+
+export async function fetchAccountsScores(
+  epoch: number,
+): Promise<MinParaBoostData[]> {
+  const { data } = await axios.get<MinParaBoostData[]>(
+    `https://api.paraswap.io/stk/paraboost/list?epoch=${epoch}`,
+  );
+  assert(
+    data.length > 0,
+    'logic error: unlikely that no paraboost was recorded',
+  );
+  return data;
+}
+
+export async function fetchTotalScore(epoch: number): Promise<string> {
+  const { data } = await axios.get<string>(
+    `https://api.paraswap.io/stk/paraboost/totalScore?epoch=${epoch}`,
+  );
+  return data;
+}
