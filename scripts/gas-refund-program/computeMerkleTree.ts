e3df49e6ee5e32ea2690a92d5bc0c5e6f7c74a2e
alexshchur <60445720+alexshchur@users.noreply.github.com>
2023-12-21 10:25:43 +0200

    chore: cleanup

--- scripts/gas-refund-program/computeMerkleTree.ts
+++ scripts/gas-refund-program/computeMerkleTree.ts
@@ -310,8 +310,7 @@ async function startComputingMerkleTreesAllChains() {
 
 startComputingMerkleTreesAllChains()
   .then(() => process.exit(0))
-  .catch(err => {
-    debugger;
+  .catch(err => {    
     logger.error('computeMerkleTreesAllChains exited with error:', err);
     process.exit(1);
   });
