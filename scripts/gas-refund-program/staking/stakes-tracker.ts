c4b9b9a540fbe77de9e92cbea3d10fe21969cb24
Mohamed Hassen Mami <mohamed@paraswap.io>
2024-01-03 13:11:49 +0100

    set ltestEpochRefunded to previous one for staking reading - to be cleaned later

--- scripts/gas-refund-program/staking/stakes-tracker.ts
+++ scripts/gas-refund-program/staking/stakes-tracker.ts
@@ -70,12 +70,13 @@ export default class StakesTracker {
   }
 
   async loadHistoricalStakes() {
-    const latestEpochRefunded = await getLatestEpochRefundedAllChains();
+    //const latestEpochRefunded = await getLatestEpochRefundedAllChains(); // FIXME
 
     const endTime = SCRIPT_START_TIME_SEC - OFFSET_CALC_TIME;
 
     // V2
     const currentEpoch = getCurrentEpoch();
+    const latestEpochRefunded = currentEpoch - 1; // FIXME
     if (currentEpoch >= GasRefundV2EpochFlip) {
       let startTimeStakeV2 = await getEpochStartCalcTime(
         latestEpochRefunded || GasRefundV2EpochFlip,
