a83be5b06edcdf56c2de98888715a2af7b5d49cf
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-07-03 07:03:29 +0000

    add augustus v6.1

--- scripts/gas-refund-program/transactions-indexing/transaction-resolver.ts
+++ scripts/gas-refund-program/transactions-indexing/transaction-resolver.ts
@@ -106,6 +106,7 @@ export const getContractAddresses = ({
   return [
     ...result,
     '0x00000000FdAC7708D0D360BDDc1bc7d097F47439'.toLowerCase(), // augustus 6.0
+    '0x000db803a70511e09da650d4c0506d0000100000'.toLowerCase(), // augustus 6.1
   ];
 };
 
