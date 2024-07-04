7e0ad74648e01b65cd1cf9464ccd5f90dde105df
alexshchur <60445720+alexshchur@users.noreply.github.com>
2024-07-04 03:29:11 +0000

    add retry logic to provider so that it doesn't spoil a whole epoch's indexing work if a single RPC request fails

--- src/lib/provider.ts
+++ src/lib/provider.ts
@@ -1,16 +1,51 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Web3Provider } from './constants';
+import { retryDecorator } from 'ts-retry-promise';
 
+// these params worked best during indexing
+const TIMEOUT_MS = 300000;
+const DELAY_MS = 2500;
+const RETRY_ATTEMPTS = 5;
+const logger = global.LOGGER('provider');
 export class Provider {
   static jsonRpcProviders: { [network: number]: JsonRpcProvider } = {};
   static getJsonRpcProvider(network: number): JsonRpcProvider {
     if (!this.jsonRpcProviders[network]) {
       if (!Web3Provider[network])
         throw new Error(`Provider not defined for network ${network}`);
-      this.jsonRpcProviders[network] = new JsonRpcProvider(
-        Web3Provider[network],
+      this.jsonRpcProviders[network] = new JsonRpcProvider({
+        url: Web3Provider[network],
+        timeout: TIMEOUT_MS,
+      });
+      this.jsonRpcProviders[network] = new Proxy(
+        this.jsonRpcProviders[network],
+        {
+          get: (target, prop) => {
+            if (prop === 'send') {
+              const fn = Reflect.get(target, prop).bind(target);
+              return async function send(
+                ...args: Parameters<JsonRpcProvider['send']>
+              ) {
+                return retryDecorator(fn, {
+                  retries: RETRY_ATTEMPTS,
+                  delay: DELAY_MS,
+                  timeout: TIMEOUT_MS,
+                  logger: msg => {
+                    logger.warn(msg.substring(0, 200), { network });
+                  },
+                })(...args);
+              };
+            }
+            return Reflect.get(target, prop);
+          },
+        },
       );
+
+      // this.jsonRpcProviders[network].on('debug', (info: any) => {
+      //   // logger.debug(info);
+      // });
     }
+
     return this.jsonRpcProviders[network];
   }
 }
