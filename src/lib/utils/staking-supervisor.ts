ec1176cca845e657dfe1bb2ca8fe1ca85b228b21
alexshchur <60445720+alexshchur@users.noreply.github.com>
2025-02-17 08:55:31 +0000

    remove unnecessary hardcode

--- src/lib/utils/staking-supervisor.ts
+++ src/lib/utils/staking-supervisor.ts
@@ -47,7 +47,7 @@ export async function fetchAccountsScores_V3(
 
       // TODO: remove this "-1" thing. Purpose of it  was - test prev distribution. For that should have been stakers in the past epoch + did transactions in the past epoch, so gotta adjust here
       // tmp: work around "epoch should be lte currentEpoch=26" (at the time of writting it's not yet 27 epoch)
-      `http://localhost:3237/paraboost/v3/list?epoch=${26}`
+      `http://localhost:3237/paraboost/v3/list?epoch=${epochv2}`
     );
     // because there's now only pooling boost and it now gets reset due to migration, it is now likely...
     // assert(
